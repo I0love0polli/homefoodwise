4ed5c55f758cfe328ebf7d3105bcee7f18b135bd
I0love0polli <115264427+I0love0polli@users.noreply.github.com>
2025-06-05 22:56:19 +0200

    finale 1
    finale

---
+++ foodwise/foodwise/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '90d33b6bed9dc0200fb25ecbf0d8644bcfc4b424',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '90d33b6bed9dc0200fb25ecbf0d8644bcfc4b424',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
